ac387a428982bc88bb750ea5e763dd1672749676
Abdullah <aalthawr@gmail.com>
2023-04-27 13:17:41 +0200

    add redirects for netlify

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,5 +1,5 @@
-export const seasonListAPI = () => `/f1/seasons.json?offset=55`; //offset 55 to start from 2005
-export const seasonStandingsAPI = (year: string) => `/f1/${year}/driverStandings.json`;
-export const seasonRaceListAPI = (year: string) => `/f1/${year}/results.json`;
+export const seasonListAPI = () => `f1/seasons.json?offset=55`; //offset 55 to start from 2005
+export const seasonStandingsAPI = (year: string) => `f1/${year}/driverStandings.json`;
+export const seasonRaceListAPI = (year: string) => `f1/${year}/results.json`;
 
-export const BASE_URL = import.meta.env.VITE_APP_API_URL || 'http://ergast.com/api';
+export const BASE_URL = import.meta.env.VITE_APP_API_URL || 'http://ergast.com/api/';
